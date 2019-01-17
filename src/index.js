835faec2aff4dd9e7f4d6e23003e51b5ad485afc
Cleyton <cleytonpessoni@gmail.com>
2019-01-17 14:04:26 -0200

    Script build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import './index.css';
 import App from './PesquisaCnpjApp';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+
+import { AppRegistry } from 'react-native';
+
+
+const rootTag = document.getElementById('root');
+
+AppRegistry.registerComponent('UniversalReactNative', () => App);
+AppRegistry.runApplication('UniversalReactNative', { rootTag });
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
